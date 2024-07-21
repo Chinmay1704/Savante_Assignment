cf0c6acff7e641a7d9fe86d690550502cf82dc24
Chinmay1704 <cpkhanapurkar1704@gmail.com>
2024-07-22 03:23:23 +0530

    home page complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import './App.css'
 import Signup from './components/signup/Signup'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
 
 function App() {
 
   return (
     <div className="App">
       <Navbar/>
+
       <Home/>
+      
+      <Footer/>
     </div>
   )
 }
